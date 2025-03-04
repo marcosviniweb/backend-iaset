554e0a86c4bbe633599422c303330621b9a395b7
MARCOS RIBEIRO <mv.r.marcosvini@gmail.com>
2025-03-04 14:02:06 -0300

    fix: porta api

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document); // URL: http://localhost:3000/api
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 
 bootstrap();
