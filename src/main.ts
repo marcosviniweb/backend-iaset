a670265fb4f5e6890e7e0046ec5177f46e92d77e
MARCOS RIBEIRO <mv.r.marcosvini@gmail.com>
2025-03-16 20:46:33 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
   // Define o prefixo '/api' para todas as rotas
